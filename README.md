46d0cfad1bbf888c0515e0c39b999961837a120d
jaymes15 <jamesoluwatomisinomotosho@gmail.com>
2022-05-28 12:15:00 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Basic Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, a sample script that deploys that contract, and an example of a task implementation, which simply lists the available accounts.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat accounts
+npx hardhat compile
+npx hardhat clean
+npx hardhat test
+npx hardhat node
+node scripts/sample-script.js
+npx hardhat help
+```
+
+RINKEBY NETWORK:
+ - contract address: 0x465EAccAEA1D3A02BbA4875aF61211a69501ee50
+
+ - Link to contract details on etherscan: https://rinkeby.etherscan.io/address/0x7f40423B12c7c101fdF19274eb033DEc5D8876b2
+
---
+++ scripts/run.js
@@ -0,0 +1,39 @@
+const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    const waveContract = await waveContractFactory.deploy();
+    await waveContract.deployed();
+    console.log("Contract deployed to:", waveContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    console.log("Contract deployed to:", waveContract.address);
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave("A message!");
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
+
+    waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
+    await waveTxn.wait();
+  
+    waveCount = await waveContract.getTotalWaves();
+
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0); // exit Node process without error
+    } catch (error) {
+      console.log(error);
+      process.exit(1); // exit Node process while indicating 'Uncaught Fatal Exception' error
+    }
+    // Read more about Node exit ('process.exit(num)') status codes here: https://stackoverflow.com/a/47163396/7974948
+  };
+  
+  runMain();
